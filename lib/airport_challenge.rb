53e1cbbb87c95de529743fa1f18b856035e39d96
Michael <Michael0ndukwe@gmail.com>
2020-12-15 16:58:25 +0000

    added user story pt3

--- lib/airport_challenge.rb
+++ lib/airport_challenge.rb
@@ -1,17 +1,22 @@
 class Airport
   def initialize
-    @hangar = []
+   @hangar = []
   end
 
   def will_land(plane)
+    fail "theres no space" if @hangar.length() >= 5
     @hangar << plane
   end
 
   def hangar
-   @hangar 
+  @hangar
   end
 
   def take_off(plane)
     @hangar.delete(plane)
+  end
+
+  def max_capacity
+  #  @hangar.length() < 6 ? @hangar << plane : fail "Theres no space"
   end
 end
